f47145c754728be35df9a3f3f10419d806d81049
Clint.Abz <abz.devs@gmail.com>
2019-11-15 22:53:53 +0800

    Noted

--- app/configs/config.php
+++ app/configs/config.php
@@ -11,10 +11,10 @@
 			define('APP_ROOT', dirname(dirname(__FILE__)));
 
 			//URL ROOT
-			define('URL_ROOT', 'https://192.168.0.35/chem');
+			define('URL_ROOT', 'https://localhost/chem');
 
 			//SITE NAME
-			define('SITE_NAME', 'Chem Management System');
+			define('SITE_NAME', 'Help Agency');
 
 			//SALT
 			define('SECURE_SALT', 'k<UL?Gxr%6bTv[IX5h>s)vaEurK]4Sn');

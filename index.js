ea6dc620327e50e781660eeac228058cad114a37
Clint.Abz <abz.devs@gmail.com>
2020-05-07 14:56:11 +0800

    CORS fixing.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ let express = require("express");
 // let moment = require("moment");
 let chalk = require("chalk");
 let app = express();
-const cors = require('cors');
-app.use(cors());
+// const cors = require('cors');
+// app.use(cors());
 
 let port = process.env.PORT || 3389;
 
 // let server = app.listen(port);
 let http = require("http").createServer(app);
-let io = require("socket.io")();
+let io = require("socket.io")(http);
 // let io = new server();
 
 http.listen(port, function () {

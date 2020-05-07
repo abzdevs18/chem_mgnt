de2459db67316f294e50d3783ab67dade61dc40d
Clint.Abz <abz.devs@gmail.com>
2020-05-07 14:34:47 +0800

    CORS fixing.

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-let express = require("express");
-// let moment = require("moment");
-let chalk = require("chalk");
-let app = express();
-const cors = require('cors');
-app.use(cors());
+var app = require('express')();
+var http = require('http').createServer(app);
 
-let port = process.env.PORT || 3000;
-
-// let server = app.listen(port);
-// let server = require("http").createServer(express);
-let server = require("socket.io");
-let io = new server();
-
-app.listen(port, function () {
-  console.log(chalk.green("Server running on: " + port));
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
 });
-// express.get("/", (req, res) => {
-//   res.send("Chat Server is running on port " + port);
-// });
-// server.listen(port, function() {
-//   console.log("Chat Server is running on port " + port);
-// });
 
-io.on("connection", function (socket) {
-  console.log("connected");
-
-  socket.on("message", function (data) {
-    console.log(data);
-    io.emit("message", data);
-  });
-
-  socket.on("notif", function (data) {
-    console.log(data);
-    io.emit("notif", data);
-  });
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
+
+// io.on("connection", function (socket) {
+//   console.log("connected");
+
+//   socket.on("message", function (data) {
+//     console.log(data);
+//     io.emit("message", data);
+//   });
+
+//   socket.on("notif", function (data) {
+//     console.log(data);
+//     io.emit("notif", data);
+//   });
+// });

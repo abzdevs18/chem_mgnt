e7ecb3f1041c2b0cb6b13323a7ce2a38aa9296aa
Clint.Abz <abz.devs@gmail.com>
2020-05-07 12:39:07 +0800

    CORS fixing.

--- index.js
+++ index.js
@@ -10,17 +10,11 @@ let port = 3000;
 
 let server = require("http").createServer(app);
 let io = require("socket.io")(server);
-io.origins((origin, callback) => {
-  if (origin !== 'http://chemlab.cf:3000') {
-    return callback('origin not allowed', false);
-  }
-  callback(null, true);
-});
-
 server.listen(port, function () {
   console.log(chalk.green("Server running on: " + port));
 });
 
+io.origins(['http://chemlab.cf:3000']);
 io.on("connection", function (socket) {
   console.log("connected");
 

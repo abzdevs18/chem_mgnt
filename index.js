f75c97c46cd7265aeb12b2d8ec61f2f9daca542f
Clint.Abz <abz.devs@gmail.com>
2020-06-21 11:30:46 +0800

    M

--- index.js
+++ index.js
@@ -34,13 +34,6 @@ let io = require("socket.io")(http);
 
 // let io = new server();
 
-http.get('*', function(req, res) {  
-  res.redirect('https://' + req.headers.host + req.url);
-
-  // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
-  // res.redirect('https://example.com' + req.url);
-})
-
 
 http.listen(port, function () {
 
